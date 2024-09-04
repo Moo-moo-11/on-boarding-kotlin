cf6f51b7c9c0612a9294575b89c3a72b5c2655b7
moomoo <anwns1211@gmail.com>
2024-09-04 16:04:48 +0900

    :tada: 프로젝트 시작!

---
+++ src/main/kotlin/moomoo/onboardingkotlin/OnBoardingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package moomoo.onboardingkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnBoardingKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnBoardingKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/moomoo/onboardingkotlin/OnBoardingKotlinApplicationTests.kt
@@ -0,0 +1,12 @@
+package moomoo.onboardingkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnBoardingKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
